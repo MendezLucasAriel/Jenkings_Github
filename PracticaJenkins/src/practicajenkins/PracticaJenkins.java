8bd5a2147b04a02e2b70fdf8350c33dece956110
Lucas Mendez <lucasarielmendez2004@gmail.com>
2023-10-02 19:51:30 -0300

    "Commit de proyecto "a commit has been made"

---
+++ PracticaJenkins/src/practicajenkins/PracticaJenkins.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicajenkins;
+
+
+/**
+ *
+ * @author teenc
+ */
+public class PracticaJenkins {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          
+        // TODO code application logic here
+         
+        System.out.println("A commit has been made");
+     
+    }
+    
+}
